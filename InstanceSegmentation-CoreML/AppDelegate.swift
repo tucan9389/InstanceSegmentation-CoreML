c797f0e710ecf311d68fe66efaba55a32f8ce095
doyoung-gwak <tucan9389@gmail.com>
2021-03-06 22:40:39 +0900

    Create initial Xcode project

---
+++ InstanceSegmentation-CoreML/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  InstanceSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 2021/03/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ InstanceSegmentation-CoreML/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  InstanceSegmentation-CoreML
+//
+//  Created by Doyoung Gwak on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
